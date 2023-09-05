fdbfa41cb7fdc159bc1dabf2417e6c250d20e04e
AndreyShuma <0662389612a@gmail.com>
2023-09-05 09:33:03 +0300

    05-09-23

--- server/servers/httpServer.js
+++ server/servers/httpServer.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const mainRouter = require('../router/main');
 const dataRouter = require( '../router/data');
+const arcticalRouter = require('../router/arctical');
 
 const server = express();
 
@@ -13,5 +14,18 @@ server.use(cors());
 
 server.use('/main', mainRouter);
 server.use('/data', dataRouter);
-
+server.use('', arcticalRouter);
 server.listen(8000, () => {console.log('Server run port 8000')});
+
+try {
+    console.log('Start');
+    const result = () => {
+        throw new Error();
+    }
+    result();
+    console.log('End');
+  } catch (error) {
+    // Код для обробки помилок
+    console.error("Помилка>>>>>>>>" + error); // Виведення повідомлення про помилку
+  } 
+  
